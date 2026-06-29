b2168353308c83161e8c00b8f85b1fdfbb7c5f7c
brayannfarfan-alt <brayann.farfan@bue.edu.ar>
2026-06-29 20:01:37 -0300

    Update helpers.py

--- pre-entrega/utils/helpers.py
+++ pre-entrega/utils/helpers.py
@@ -2,16 +2,21 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 import json
 
-
+options = Options()
 
 def get_driver():
     service = Service(ChromeDriverManager().install())
+    optiuons.add_argument("--headless")
     driver = webdriver.Chrome(service=service)
+
+    
+    
     return driver
 
 
